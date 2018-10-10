976c7d7bcac122e2e5548f7d2e9e62cfcff6a733
Bruno Marques <profbrunomarques@gmail.com>
2018-10-10 20:50:41 -0300

    Criação da classe Page responsável por gerar o Template View das páginas

---
+++ vendor/blog/classes/Page.php
@@ -0,0 +1,60 @@
+<?php
+namespace Blog;
+
+use Rain\Tpl;
+
+class Page {
+    
+    private $tpl;
+    private $options = [];
+    //Valor padrão caso não seja passado $opts
+    private $defaults = [
+        "data"=>[]
+    ];
+
+    /**
+     * Método contrutor que irá criar o Header do Template View.
+     * Este médodo 
+     */
+    public function __construct($opts = array()){
+        //Casso seja enviada as opções, será feito o merge do default com o que foi enviado em $opts.
+        //Essas $opts são variáveis que serão substituidas no HTML nesse formato {$nome}
+        $this->options = array_merge($this->defaults, $opts);
+	    $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false // set to false to improve the speed
+        );
+
+        Tpl::configure( $config );
+        $this->tpl = new Tpl;
+        $this->setData($this->options["data"]);
+        //Desenha o template do header.html, que está na pasta views e renderiza na tela.
+        $this->tpl->draw("header");
+        
+    }
+    private function setData($data = array ())
+    {
+        //faz a atribuição das variáveis
+        foreach ($data as $key => $value) {
+            $this->tpl->assign($key, $value);
+        }
+    }
+    // Corpo da página
+    /**
+     * Renderiza o corpo da página
+     * @param string $name = Nome do arquivo que será renderizado no corpo da página.
+     * @param array $data = Variáveis que serão substituidas.
+     * @param bool $returnHTML = Se o retorno será ou não um HTML.
+     */
+    public function setTpl($name, $data = array(), $returnHTML = false)
+    {
+        $this->setData($data);
+        return $this->tpl->draw($name, $returnHTML);
+    }
+
+    public function __destruct(){
+        //Desenha o template do footer.html, que está na pasta views e renderiza na tela.
+        $this->tpl->draw("footer");
+    }
+}
