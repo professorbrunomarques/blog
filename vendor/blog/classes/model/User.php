41a3bd70ba08115f730d0f1f05d58b5b5d79139b
Bruno Marques <profbrunomarques@gmail.com>
2018-10-29 23:17:19 -0300

    Implementando esqueci a senha. (em conclusão)

--- vendor/blog/classes/model/User.php
+++ vendor/blog/classes/model/User.php
@@ -5,11 +5,13 @@ namespace Blog\Model;
 use \Blog\Model;
 use \Blog\helper\Check;
 use \Blog\DB\Sql;
+use \Blog\Mailer;
 
 class User extends Model {
 
 	const SESSION = "User";
-
+	const SECRET = "123qwe"; 
+    const CIPHER = "AES-256-CBC";
 	
 	public static function login($login, $password):User
 	{
@@ -118,6 +120,57 @@ class User extends Model {
 		return $sql->query("DELETE FROM tb_users WHERE id_user = :id_user", array(
 			":id_user"=>$id_user
 		));
+	}
+
+	public static function getForgot(string $email){
+
+		$sql = new Sql();
+		$result = $sql->select("SELECT * FROM tb_users WHERE email = :email", array(
+			":email"=>$email 
+		));
+		if(count($result) === 0){
+			throw new \Exception("Não foi possível recuperar sua senha");			
+		}else{
+			$data = $result[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :user_ip);", array(
+				":iduser"=>$data["id_user"],
+				":user_ip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if(count($results2) === 0){
+				throw new \Exception("Não foi possível recuperar sua senha");
+			}else{
+
+				$dataRecovery = $results2[0];
+
+				$IV = random_bytes(openssl_cipher_iv_length(User::CIPHER)); 
+				//Codigo encriptografado
+				$code = openssl_encrypt($dataRecovery["idrecovery"], User::CIPHER, USER::SECRET, 0, $IV);
+
+				$result = base64_encode($IV.$code);
+				
+				$link = "http://localhost/admin/forgot/reset?code=$result";
+
+				$mailer = new Mailer($data["email"], $data["name"], "Redefinir Senha do Blog", "forgot", array(
+					"name"=> $data["name"],
+					"link"=> $link
+				));
+
+				$mailer->send();
+
+				return $data;
+			}
+		}
+	}
+
+	public static function validForgotDecrypt($result)
+	{
+		$result = base64_decode($result);
+		$code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
+		$iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');
+		$idrecovery = openssl_decrypt($code, 'aes-256-cbc', User::SECRET, 0, $iv);
+		return $idrecovery;
 	}
 
 }
